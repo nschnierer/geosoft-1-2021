1a9e59d7b9459d628270371eaeda3bc0e8cd7efa
Noel Schnierer <noel.schnierer@gmail.com>
2021-05-31 23:30:22 +0200

    add header and new page about

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { HashRouter as Router } from "react-router-dom";
+import "./tailwind.generated.css";
 import "./styles.css";
 import { App } from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Providers = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
+ReactDOM.render(<Providers />, document.getElementById("root"));
